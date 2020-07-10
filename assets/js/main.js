d1d2999a18629bc7bdda89ec0d9ee0ef8cac3aa5
Netacci <52791481+Netacci@users.noreply.github.com>
2020-07-10 16:12:24 +0100

    fixed nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 const topScroll = document.querySelector(".top-scroll");
 const nav = document.querySelector(".navbar");
 
+// backTotop scroll
 window.addEventListener("scroll", () => {
   if (window.pageYOffset > 50) {
     topScroll.classList.add("activescroll");
@@ -10,12 +11,12 @@ window.addEventListener("scroll", () => {
     nav.classList.remove("navscroll");
   }
 });
-
+//preloader
 $(window).on("load", function () {
   $(".status").fadeOut();
   $("#preloader").delay(500).fadeOut();
 });
-
+//collapse nav window
 $(document).on("click", ".navbar-collapse", function (e) {
   if ($(e.target).is("a")) {
     $(this).collapse("hide");
