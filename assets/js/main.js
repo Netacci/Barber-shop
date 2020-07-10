17c41b58b157568199728a6409f3bfb3780dee5b
Netacci <52791481+Netacci@users.noreply.github.com>
2020-07-10 08:23:36 +0100

    fixed issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,17 @@
+const topScroll = document.querySelector(".top-scroll");
+const nav = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 50) {
+    topScroll.classList.add("activescroll");
+    nav.classList.add("navscroll");
+  } else {
+    topScroll.classList.remove("activescroll");
+    nav.classList.remove("navscroll");
+  }
+});
+
 $(window).on("load", function () {
   $(".status").fadeOut();
   $("#preloader").delay(500).fadeOut();
-  // $(".navbar-brand").addClass("animate__animated animate__bounce");
 });
